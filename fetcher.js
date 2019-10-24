4b4a542745150243c3d6f2f714b32f98a67a06e8
granttaylor448 <granttaylor448@gmail.com>
2019-10-24 15:16:42 +0000

    pushing worked page-fetcher code that meets all bot one condition of the stretch conditions

--- fetcher.js
+++ fetcher.js
@@ -1,16 +1,34 @@
 const args = process.argv.slice(2)
 const fs = require('fs')
 var readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
 const request = require('request');
 
-request(args[0], (error, response, body) => {
-  fs.writeFile( args[1], body, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved');
-  });
-});
+// request(args[0], (error, response, body) => {
+  // 
+  // fs.writeFile( args[1], body, (err) => {
+    // if (err) throw err;
+    // else {
+      // if (fs.existsSync(args[1])) {
+      // rl.question('Do you want to overwrite this file?', (answer) => {
+        // if (answer === "yes") {
+          // console.log("You got it")
+          // rl.close()
+        // } else {
+          // rl.close()
+        // }
+      // 
+      // });
+    // }
+    //  };
+    // console.log('The file has been saved');
+  // });
+// });
 
 // const data = new Uint8Array(Buffer.from(request()));
 
@@ -22,3 +40,45 @@ request(args[0], (error, response, body) => {
 // setTimeout(() => {
   // writer
 // }, 1000);
+
+request(args[0], (error, response, body) => {
+  let string = args[1]
+//  if (string.slice((string.length - 4[ string.length] !== "html"))) {
+  // console.log(string.slice((string.length - 4[ string.length] !== "html")))
+  // rl.close()
+  // return;
+//  }
+  
+  if (body === undefined){
+    console.log("Whoops that's not a website")
+    rl.close()
+    return;
+    
+  }
+  if (fs.existsSync(args[1])) {
+    
+    rl.question('Do you want to overwrite this file?', (answer) => {
+      if (answer === "yes") {
+        console.log("You got it")
+        fs.writeFile( args[1], body, () => {
+          console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`)
+          rl.close()
+        })
+      } else {
+        console.log("suit yourself")
+        rl.close()
+      }
+      })
+      } else {
+      
+        fs.writeFile(args[1], body, (err) => {
+          if (err) { console.log("Not the right file path");
+          rl.close()
+        } else { 
+          
+          console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`)
+          rl.close()
+        }
+        })
+      }
+    });
